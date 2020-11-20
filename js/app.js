6b2b6b00f85c68f641bcc028de99f7ac83567075
janxavierv <37735268+janxavierv@users.noreply.github.com>
2020-11-20 21:31:51 +1100

    Exceeds Expectations created

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
 baguetteBox.run('.gallery',{
     animation:'fade-in'
 });
 
+// Used TeamTreeHouse's Project Resources.
+ function search(){
+    //  The following variables selects html tags and IDs
+    let input = document.getElementById("search");
+    let filter = input.value.toUpperCase();
+    let div = document.getElementById("gallery");
+    let a = div.getElementsByTagName("a");
+
+    // Loops through an HTML Collection stored in the "a" variable
+    for(let i=0; i < a.length; i++) {
+        //Selects "a" tags with an attribute of data-collection and sets all inputs and text to uppercase.
+        img = a[i].getAttribute("data-caption").toUpperCase();
+        if(img.includes(filter)) {
+            a[i].style.display="";
+        }else{
+            a[i].style.display="none";
+        }
+    } 
+}
